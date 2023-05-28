a7952e68173d581bf0e9c16eb01065aa2f4dac46
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2023-05-29 03:09:36 +0900

    SQL追加

--- db/initdb.d/sample.sql
+++ db/initdb.d/sample.sql
@@ -26,4 +26,20 @@ INSERT INTO `article` (`id`, `user_name`, `title`, `short_message`, `text`, `cre
 (1,	'aaa',	'aaa',	'aaa',	'aaaa',	'2023-03-20 01:35:39',	'2023-03-20 01:35:39'),
 (2,	'sasasa',	'sasa',	'sasa',	'sasasasa;:l;.,x.x,vx:v',	'2023-03-20 01:56:28',	'2023-03-20 01:56:28');
 
+DROP TABLE IF EXISTS `article1`;
+CREATE TABLE `article1` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(191) COLLATE utf8mb4_bin NOT NULL,
+  `title` varchar(191) COLLATE utf8mb4_bin NOT NULL,
+  `short_message` varchar(191) COLLATE utf8mb4_bin NOT NULL,
+  `text` text COLLATE utf8mb4_bin NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+INSERT INTO `article1` (`id`, `user_name`, `title`, `short_message`, `text`, `created`, `modified`) VALUES
+(1,	'aaa',	'aaa',	'aaa',	'aaaa',	'2023-03-20 01:35:39',	'2023-03-20 01:35:39'),
+(2,	'sasasa',	'sasa',	'sasa',	'sasasasa;:l;.,x.x,vx:v',	'2023-03-20 01:56:28',	'2023-03-20 01:56:28');
+
 -- 2023-03-21 15:43:10
